7a45c1696f525ffeef4efa885d370d4e2ebc4cee
Jigar Bhanushali <jigard.bhanushali12@gmail.com>
2023-12-21 19:30:44 +0530

    fixed: cors method

--- utils/cors.js
+++ utils/cors.js
@@ -3,7 +3,7 @@ import Cors from 'cors';
 
 // Initializing the cors middleware
 const cors = Cors({
-    methods: ['GET', 'HEAD', 'POST', 'OPTIONS'],
+    methods: ['*'],
 });
 
 // Helper method to wait for a middleware to execute before continuing

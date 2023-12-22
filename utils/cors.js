5cbb425df64c285377683d6a75e15d7ad43c82dd
Jigar Bhanushali <jigard.bhanushali12@gmail.com>
2023-12-22 11:41:53 +0530

    fix: allowed methods in cors

--- utils/cors.js
+++ utils/cors.js
@@ -3,7 +3,7 @@ import Cors from 'cors';
 
 // Initializing the cors middleware
 const cors = Cors({
-    methods: ['GET', 'HEAD', 'POST', 'OPTIONS', "PUT", "DELETE", "PATCH"],
+    methods: ['GET', 'HEAD', 'POST', 'OPTIONS', 'PUT', 'DELETE', 'PATCH'],
 });
 
 // Helper method to wait for a middleware to execute before continuing

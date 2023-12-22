141857f9bcdd876e96657acb4a5205c95f8021c1
Jigar Bhanushali <jigard.bhanushali12@gmail.com>
2023-12-22 11:26:33 +0530

    fix: api deployment

--- utils/config.js
+++ utils/config.js
@@ -5,5 +5,5 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const API_BASE_URL = dev
     ? 'http://localhost:3000/api/'
-    : 'https://next-js-udemy-file-route-app.vercel.app/api';
+    : 'https://next-js-udemy-file-route-app.vercel.app/api/';
 
